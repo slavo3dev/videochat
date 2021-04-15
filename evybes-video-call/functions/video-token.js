45c735c3017717da8bad812bcd79e40cbfaace87
slavo3dev <slavo@slavo3.com>
2021-04-15 10:43:53 -0400

    adding video grant

--- evybes-video-call/functions/video-token.js
+++ evybes-video-call/functions/video-token.js
@@ -1,12 +1,21 @@
-exports.handler = function ( context, event, callback )
-{
-    const TWILIO_ACCOUNT_SID = context.TWILIO_ACCOUNT_SID;
-    const TWILIO_API_KEY = context.TWILIO_API_KEY;
-    const TWILIO_API_SECRET = context.TWILIO_API_SECRET;
+exports.handler = function (context, event, callback) {
+  const TWILIO_ACCOUNT_SID = context.ACCOUNT_SID;
+  const TWILIO_API_KEY = context.API_KEY;
+  const TWILIO_API_SECRET = context.API_SECRET;
+  const AccessToken = Twilio.jwt.AccessToken;
 
-    const AccessToken = TWILIO.jwt.AccessToken;
-
-    const accessToken = new AccessToken( TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET );
-
-    callback( null, { token: accessToken.toJwt() } );
+  const accessToken = new AccessToken(
+    TWILIO_ACCOUNT_SID,
+    TWILIO_API_KEY,
+    TWILIO_API_SECRET
+  );
+    
+    const VideoGrant = AccessToken.VideoGrant()
+    const videoGrant = new VideoGrant( {
+        room: event.room
+    } )
+    
+    accessToken.addGrant( videoGrant );
+    
+    callback(null, { token: accessToken.toJwt() });
 }
